d84d759faa6171aad150d77858f7079adf539870
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2025-02-10 22:16:39 -0300

    Inicio Controle de Versão

---
+++ src/main/java/com/itb/inf3bn/pizzariacurso25/Pizzariacurso25Application.java
@@ -0,0 +1,13 @@
+package com.itb.inf3bn.pizzariacurso25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pizzariacurso25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pizzariacurso25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/inf3bn/pizzariacurso25/Pizzariacurso25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf3bn.pizzariacurso25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pizzariacurso25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
